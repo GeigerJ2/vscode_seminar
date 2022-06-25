777a1459930904859165a021615e8e29c71e99c0
GeigerJ2 <jgeiger@iciq.es>
2022-06-25 20:53:41 +0200

    First commit

---
+++ bash_file.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+for metal in cu ag au; do
+    echo $metal # $other_metal
+done
+
+# test
+
+# for metal in cu ag au; do
+#     if [[ "${metal}" == 'au' ]]; then
+#         echo "Hell yeah"
+#     fi
+# done
---
+++ python_file.py
@@ -0,0 +1,48 @@
+import os
+
+import numpy as np
+import pandas as pd
+from ase import Atoms
+from ase.visualize import view
+
+from ase.io.vasp_parsers.vasp_outcar_parsers import Cell
+# ? Why am I even importing this?
+# ! Because it is important.
+# TODO: But there are still other todos.
+
+
+df = pd.DataFrame()
+
+
+class Human:
+    def __init__():
+        pass
+
+
+human = Human()
+
+########## ASE example ##########
+
+# Easy example
+a = 4.05  # Gold lattice constant
+b = a / 2
+au_fcc = Atoms("Au", cell=[(0, b, b), (b, 0, b), (b, b, 0)], pbc=True)
+
+
+########## Automatic code formatting with black ##########
+# Stolen from yapf-GitHub: https://github.com/google/yapf
+
+# x = {  'a':37,'b':42,
+
+# 'c':926}
+
+# y = 'hello ''word'
+# z = 'hello '+'world'
+# a = 'hello {}'.format('word')
+# class foo  (     object  ):
+#   def f    (self   ):
+#     return       37*-+2
+#   def g(self, x,y=42):
+#       return y
+# def f  (   a ) :
+#   return      37+-+a[42-x :  y**3]
